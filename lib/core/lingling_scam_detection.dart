76775577623d5df304ecdbda6a4d53796e784042
钟智强 <johnmelodymel@qq.com>
2025-07-11 02:22:26 +0800

    feat(contacts): 添加联系人模型和诈骗检测功能
    添加uuid依赖用于生成唯一标识
    扩展Contacts模型并添加唯一标识字段
    为LinglingScamDetection添加数据库操作接口

--- lib/core/lingling_scam_detection.dart
+++ lib/core/lingling_scam_detection.dart
@@ -1,3 +1,26 @@
+import 'package:ling_ling_app/models/contacts.dart';
+
 abstract class LinglingScamDetection {
+  /// 数据库操作接口
+  ///
+  /// [phoneNumber] 电话号码查询记录数
   Future<int> recordCount(String phoneNumber);
+
+  /// 将联系人信息写入数据库
+  ///
+  /// [contacts] 要写入的联系人对象
+  Future<void> writeIntoDatabase(Contacts contacts);
+
+  /// 获取诈骗联系人列表
+  ///
+  /// [pagination] 分页数量，默认为30条
+  /// 返回联系人列表
+  Future<List<Contacts>> listScammersContacts({int pagination = 30});
+
+  /// 请求从数据库中删除联系人
+  ///
+  /// [contacts] 要删除的联系人对象
+  Future<void> requestToDeleteFromDatabase(Contacts contacts);
+
+  Future<void> flagContactAsScam(Contacts contacts);
 }
--- lib/models/contacts.dart
+++ lib/models/contacts.dart
@@ -1,25 +1,36 @@
+import 'package:uuid/uuid.dart';
+
 class Contacts {
   String? name;
   String? email;
   String? phoneNumber;
   String? country;
+  final String uniqueScammerId;
 
-  Contacts({this.name, this.email, this.phoneNumber, this.country});
+  Contacts({
+    this.name,
+    this.email,
+    this.phoneNumber,
+    this.country,
+    String? uniqueScammerId,
+  }) : uniqueScammerId = uniqueScammerId ?? const Uuid().v4();
 
-  Contacts.fromJson(Map<String, dynamic> json) {
-    name = json['name'];
-    email = json['email'];
-    phoneNumber = json['phoneNumber'];
-    country = json['country'];
-  }
+  factory Contacts.fromJson(Map<String, dynamic> json) => Contacts(
+    name: json['name'],
+    email: json['email'],
+    phoneNumber: json['phoneNumber'],
+    country: json['country'],
+    uniqueScammerId: json['uniqueScammerId'],
+  );
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
+  Map<String, dynamic> toJson() => {
+    'name': name,
+    'email': email,
+    'phoneNumber': phoneNumber,
+    'country': country,
+    'uniqueScammerId': uniqueScammerId,
+  };
 
-    data['name'] = name;
-    data['email'] = email;
-    data['phoneNumber'] = phoneNumber;
-    data['country'] = country;
-    return data;
-  }
+  @override
+  String toString() => 'Contacts($name, $phoneNumber, $uniqueScammerId)';
 }
