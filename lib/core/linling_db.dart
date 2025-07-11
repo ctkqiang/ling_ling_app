e0d9072e05d41cf8e569b0963f618ebcd55f3a49
钟智强 <johnmelodymel@qq.com>
2025-07-12 01:45:00 +0800

    feat: 添加删除和更新诈骗者数据的方法

--- lib/core/linling_db.dart
+++ lib/core/linling_db.dart
@@ -1,8 +1,9 @@
 import 'package:ling_ling_app/models/database/scammers_data.dart';
-import 'package:sqflite/sqflite.dart';
 
 abstract class LingLingDb {
   Future<void> getUserCountryCode();
   Future<List<ScammersData>> getScammersData();
   Future<int> insert(ScammersData scammerData);
+  Future<int> delete(ScammersData scammerData);
+  Future<int> update(ScammersData scammerData);
 }
