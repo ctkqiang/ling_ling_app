8fd17b7506da95e4f6e9d4dd8b1b7eae7c609d39
钟智强 <johnmelodymel@qq.com>
2025-07-12 03:33:22 +0800

    feat(页面): 在工具箱页面添加返回按钮和样式优化
    添加返回按钮并优化标题样式，使用GetX进行导航控制

--- lib/presentation/pages/fragments/tool_box_page.dart
+++ lib/presentation/pages/fragments/tool_box_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:ling_ling_app/controller/database_controller.dart';
 
 class ToolBox extends StatefulWidget {
@@ -23,7 +24,27 @@ class _ToolBoxState extends State<ToolBox> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(databaseController.username.value)),
+      appBar: AppBar(
+        leading: GestureDetector(
+          child: Icon(
+            Icons.arrow_back,
+            color: Theme.of(context).colorScheme.secondaryContainer,
+          ),
+          onTap: () => Get.back(),
+        ),
+        centerTitle: true,
+        bottomOpacity: 0.0,
+        elevation: 0.0,
+        title: Obx(
+          () => Text(
+            databaseController.username.value,
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.secondaryContainer,
+              fontSize: 20,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
